08f213ad5beefe222c056fd5b02652dbe455253f
anhphamngo <144450186+anhphamngo@users.noreply.github.com>
2023-09-26 21:15:10 +0700

    Update traning.py

--- lib/traning.py
+++ lib/traning.py
@@ -1,3 +1,11 @@
 code = ['code', 'studio', 'open visual studio']
+code_path = "C:\\Users\\anhph\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe\\"
+
+close = ["goodbye", "bye", "see you", "see you again"]
+
+timec = ["what time", "what is the time"]
+
+greet = ["hello", "greet", "hi"]
+
 waiting = ['I\'m here!', 'Uhm...', 'Lalala.', 'Try say "nano"']
 msekill = []
